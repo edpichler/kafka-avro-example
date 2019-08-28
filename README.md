f2eed51d3efd8db0282cbd6f0993173bfee4651b
Eduardo Ivan Pichler <eduardopichler@gmail.com>
2019-08-28 13:47:54 +0200

    Avro working!

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
-This is a SpringBoot application that publishes and consumes Kafka events.
+This is a SpringBoot application that publishes and consumes objects/events.
 
-For serializing and deseriasazing uses Apache Avro.
+For serializing and deserializing it uses Apache Avro.
 
-Main class is KafkaAvroExampleApplicationiyf.
-Class SchemaBuilder is a helper to generate the Avro schemas.  
+To publish and consume it uses kafka.
+
+Main class is ```KafkaAvroExampleApplicationiy```.
+
+The Avro kotlin/java objects are generated in compilation time by the ```generateAvroJava``` task of Gradle. To do that, it uses the schema contained on the folder ```src/main/avro/```. 
--- src/main/kotlin/com/pichler/examples/avro/Producer.kt
+++ src/main/kotlin/com/pichler/examples/avro/Producer.kt
@@ -2,28 +2,28 @@ package com.pichler.examples.avro
 
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
-import java.io.IOException
-import org.apache.commons.compress.utils.IOUtils.toByteArray
 import org.apache.avro.io.EncoderFactory
 import java.io.ByteArrayOutputStream
 import org.apache.avro.specific.SpecificDatumWriter
-import org.apache.avro.io.DatumWriter
-import org.apache.avro.io.Encoder
 
 
 @Component
 class Producer {
 
+    var count = 0
     @Scheduled(fixedRate = 1000)
-    fun todo() {
-        val writer = SpecificDatumWriter(MyCustomObject::class.java)
-        val stream = ByteArrayOutputStream()
-        var jsonEncoder: Encoder? = null
-        jsonEncoder = EncoderFactory.get().jsonEncoder(
-                MyCustomObject.getClassSchema(), stream)
-        jsonEncoder!!.flush()
+    fun publish() {
 
-        Queue.add(stream.toString())
-        println(stream.toString())
+        val toSerialize = MyCustomObject.newBuilder().setName("my name").setDetails("Object number " + count++).build()
+
+        val writer = SpecificDatumWriter(MyCustomObject::class.java)
+
+        val outStream = ByteArrayOutputStream()
+        var encoder = EncoderFactory.get().binaryEncoder(outStream, null)
+        writer.write(toSerialize, encoder)
+        encoder!!.flush()
+
+        val serializedObject = outStream.toString()
+        Queue.add(serializedObject)
     }
 }
